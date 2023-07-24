661b1b0f87b60333baa28f02372ed773d0c05cdc
drumil ved <drumilved@drumils-MacBook-Pro.local>
2023-07-24 17:46:24 +0530

    teams page first draft

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
-import Nav from '@/components/Nav'
-import Image from 'next/image'
+// import Nav from "@/components/Nav";
+import Nav from "../components/Nav";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Nav/>
+      <Nav />
       RSM Website
     </main>
-  )
+  );
 }
