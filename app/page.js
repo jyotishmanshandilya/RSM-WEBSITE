c56c055b3e4ca28b9f38df364967f363d3f92919
drumil ved <drumilved@drumils-MacBook-Pro.local>
2023-07-25 10:17:44 +0530

    corrected package.json

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 // import Nav from "@/components/Nav";
+"use client";
 import Nav from "../components/Nav";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,6 +9,8 @@ export default function Home() {
     <main>
       <Nav />
       RSM Website
+      {/* <ParticleBG /> */}
+      <Nav />
     </main>
   );
 }
