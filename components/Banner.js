a6fd8d527e3a1d25a24b1e05bc18127cac60cb28
Jyotishman Shandilya <jyotishmanshandilyaggn@gmail.com>
2023-07-27 22:58:04 +0530

    banner update

--- components/Banner.js
+++ components/Banner.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <section className='pt-20 mt-10 bg-[#1c1c1ce1] opacity-90'>
-      <h1 className='text-white text-center lg:p-24 lg:font-medium font-thin p-10'>
+    <section className='lg:pt-20 mt-10 bg-[#1c1c1ce1] opacity-90 py-24'>
+      <h1 className='text-white text-center lg:p-24 font-thin p-10'>
         <span className='text-5xl'>Welcome to</span>
         <br/>
         <span>The Research Society</span>
