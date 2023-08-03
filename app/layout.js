361918c80a9aaa3eabf82d21ca8f8433fca93504
Jyotishman Shandilya <jyotishmanshandilyaggn@gmail.com>
2023-08-03 20:59:11 +0530

    added official font

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,22 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google';
 import ParticleBG from '../components/ParticleBG';
 import Nav from "../components/Nav";
 import Footer from '../components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'RSM Offical website',
+  title: 'Research Society Manipal | The Official Student Research Body of Manipal Institute Of Technology, Manipal',
   description: 'Next.js website to showcase the clubs activites, events, members, contact info and other information',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <Nav/>
         {children}
         <ParticleBG />
--- components/config/particle-config.js
+++ components/config/particle-config.js
@@ -1,4 +1,5 @@
-const particlesConfig = {
+const particlesConfig = 
+{
   "particles": {
     "number": {
       "value": 150,
@@ -103,5 +104,53 @@ const particlesConfig = {
   },
   "retina_detect": true
 }
+
+// {
+//   particles: {
+//     color: '#fff',
+//     shape: 'circle', // "circle", "edge" or "triangle"
+//     opacity: 1,
+//     size: 4,
+//     size_random: true,
+//     nb: 150,
+//     line_linked: {
+//       enable_auto: true,
+//       distance: 100,
+//       color: '#fff',
+//       opacity: 1,
+//       width: 1,
+//       condensed_mode: {
+//         enable: false,
+//         rotateX: 600,
+//         rotateY: 600
+//       }
+//     },
+//     anim: {
+//       enable: true,
+//       speed: 1
+//     }
+//   },
+//   interactivity: {
+//     enable: true,
+//     mouse: {
+//       distance: 250
+//     },
+//     detect_on: 'canvas', // "canvas" or "window"
+//     mode: 'grab',
+//     line_linked: {
+//       opacity: .5
+//     },
+//     events: {
+//       onclick: {
+//         enable: true,
+//         mode: 'push', // "push" or "remove" (particles)
+//         nb: 4
+//       }
+//     } 
+//   },
+//   /* Retina Display Support */
+//   retina_detect: true
+// }
+
 
 export default particlesConfig;
